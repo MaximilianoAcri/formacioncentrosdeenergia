f385cb637935d6c2203b595b27bd35f3b9c4e5c8
unknown <maximilianoacri1@gmail.com>
2023-04-17 22:04:18 -0300

    todo

---
+++ assets/js/sweetAlert.js
@@ -0,0 +1,14 @@
+Swal.fire({
+  imageUrl: 'assets/img/taller (16).png',
+  backdrop:true,
+  timer: 15000,
+  allowOutsideClick: false,
+  padding: 0,
+  paddingTitle: 0,
+  showConfirmButton:true,
+  confirmButtonColor: '#ab0af5',
+  title: '¡Aprovecha esta oportunidad!',
+  footer:'Escribenos por WhatsApp'
+
+
+})
